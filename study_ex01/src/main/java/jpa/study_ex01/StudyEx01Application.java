c1b1ecef47e9602ce5963e11a71bf0b568e5a937
Kim Jisung <49260265+d483ulert@users.noreply.github.com>
2022-07-24 14:57:51 +0900

    Add files via upload

---
+++ study_ex01/src/main/java/jpa/study_ex01/StudyEx01Application.java
@@ -0,0 +1,13 @@
+package jpa.study_ex01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyEx01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyEx01Application.class, args);
+	}
+
+}
