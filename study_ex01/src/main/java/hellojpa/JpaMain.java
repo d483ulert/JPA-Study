8e488a9041be4680d25da23fa3e8a37720ec9fa2
jisung324 <jisung3245@gmail.com>
2022-08-11 21:30:57 +0900

    jpa proxy

--- study_ex01/src/main/java/hellojpa/JpaMain.java
+++ study_ex01/src/main/java/hellojpa/JpaMain.java
@@ -1,8 +1,16 @@
 package hellojpa;
 
 
+
 public class JpaMain {
     public static void main(String[] args){
+        Member member = new Member();member.setCreatedBy("creator");
+        em.persist(member);
+        em.flush();em.clear();
+        Member findMember = em.find(Member.class, member.getId());
+        System.out.println("findMember.id = " + findMember.getId());
+        System.out.println("findMember.username = " + findMember.getUsername());
+        tx.commit();
 
         //양방향 매핑일때는 양쪽에 값을 넣어주는게 맞음
     }
--- study_ex01/src/main/java/jpabook/jpashop/domian/BaseEntity.java
+++ study_ex01/src/main/java/jpabook/jpashop/domian/BaseEntity.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Data
-public class BaseEntity {
+public abstract class BaseEntity {
 
     private String createBy;
     private LocalDateTime createDate;
