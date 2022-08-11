8505bcc0c7cb94bda047f4ed14820d98f763c53e
jisung324 <jisung3245@gmail.com>
2022-08-11 22:10:20 +0900

    jpa CaseCade All

---
+++ study_ex01/src/main/java/jpabook/jpashop/domian/Parent.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.domian;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity @Data
+public class Parent {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    private List<Child> childList = new ArrayList<>();
+
+
+    public void addChild(Child child){
+        childList.add(child);
+        child.setParent(this);
+
+    }
+}
