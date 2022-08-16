1ee282d105b1f7ee36487a9cf7a011750a915506
jisung324 <jisung3245@gmail.com>
2022-08-16 22:08:21 +0900

    프로젝션

--- study_ex01/src/main/java/hellojpa/JpaMain.java
+++ study_ex01/src/main/java/hellojpa/JpaMain.java
@@ -1,8 +1,11 @@
 package hellojpa;
+import jpabook.jpashop.domian.Member;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args){
@@ -14,6 +17,12 @@ public class JpaMain {
         tx.begin();
 
         try{
+
+            List resultList = (List) em.createQuery("select m.username,m.age from Member m").getResultList();
+            Object o = resultList.get(0);
+            Object [] result = (Object[]) o;
+            System.out.println(result[0]);
+            System.out.println(result[1]);
 
         }
         catch(Exception e) {
