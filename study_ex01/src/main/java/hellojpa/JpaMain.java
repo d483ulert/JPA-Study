2cc714805a434b49e6c69f5d3e87875e2c550299
jisung324 <jisung3245@gmail.com>
2022-08-18 22:48:54 +0900

    묵시적내부조인 발생하게 짜면 안좋음. 무조건 명시적 조인 사용하는게 좋음

--- study_ex01/src/main/java/hellojpa/JpaMain.java
+++ study_ex01/src/main/java/hellojpa/JpaMain.java
@@ -23,16 +23,13 @@ public class JpaMain {
             member.setAge(10);
             member.setMemberType(MemberType.ADMIN);
 
-            String query = "select " +
-                            "case when m.age <= 10 then '학생요금'" +
-                                "when m.age >= 60 then '경로요금'" +
-                                "else '일반요금'" +
-                            "end" +
-                            "from Member m" ;
-            List<String> result =em.createQuery(query,String.class).getResultList();
+            //
 
-            String query2 = "select coalesce(m.username,'이름 없는 회원') from Member m";
 
+            String query2 = "select m from Member m where m.id = :memberId";
+            Member findMember = em.createQuery(query2,Member.class)
+                    .setParameter("memberId",member.getId())
+                    .getSingleResult();
         }
         catch(Exception e) {
             e.printStackTrace();
