22504c1ed9b00931e4b0566de3b9554e552043d1
jisung324 <jisung3245@gmail.com>
2022-08-26 21:34:51 +0900

    p6spy설정 추가 및 tdd코드작성

---
+++ study_ex01/src/main/java/jpa/study_ex01/MemberRepository.java
@@ -0,0 +1,23 @@
+package jpa.study_ex01;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    //테스트코드 만들기 쉬프트+컨트롤+T
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class,id);
+    }
+}
