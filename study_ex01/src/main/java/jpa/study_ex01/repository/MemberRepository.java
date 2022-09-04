631a7b5ce8b8d51f6d2f94a4944e91879dd16837
jisung324 <jisung3245@gmail.com>
2022-09-04 22:38:43 +0900

    Member Repository 추가

--- study_ex01/src/main/java/jpa/study_ex01/repository/MemberRepository.java
+++ study_ex01/src/main/java/jpa/study_ex01/repository/MemberRepository.java
@@ -5,19 +5,31 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 public class MemberRepository {
     @PersistenceContext
     private EntityManager em;
 
-    //테스트코드 만들기 쉬프트+컨트롤+T
-    public Long save(Member member){
+    public void save(Member member){
         em.persist(member);
-        return member.getId();
+    }
+    //테스트코드 만들기 쉬프트+컨트롤+T
+
+
+    public Member findOne(Long id){
+       return em.find(Member.class,id);
     }
 
-    public Member find(Long id){
-        return em.find(Member.class,id);
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m",Member.class).getResultList();
+    }
+
+    public List<Member> findByName(String name){
+        return em.createQuery("select m from Member m where m.name= :name",Member.class)
+                .setParameter("name",name)
+                .getResultList();
     }
 }
+
--- study_ex01/src/test/java/jpa/study_ex01/MemberRepositoryTest.java
+++ study_ex01/src/test/java/jpa/study_ex01/MemberRepositoryTest.java
@@ -18,20 +18,4 @@ class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
 
-    @Test
-    @Transactional
-    public void testMember()throws Exception{
-        //given
-        Member member = new Member();
-        member.setName("memberA");
-
-        //when
-        Long saveId = memberRepository.save(member);
-        Member findMember = memberRepository.find(saveId);
-
-        //then
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getName());
-        Assertions.assertThat(findMember).isEqualTo(member);
-    }
 }
