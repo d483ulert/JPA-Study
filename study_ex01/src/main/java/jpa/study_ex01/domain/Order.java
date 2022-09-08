51c4db96ee927b4fea7995dea531083542078c9a
jisung324 <jisung3245@gmail.com>
2022-09-08 21:35:11 +0900

    주문 entity 서비스 로직, 비즈니스로직 생성메서드 추가

--- study_ex01/src/main/java/jpa/study_ex01/domain/Order.java
+++ study_ex01/src/main/java/jpa/study_ex01/domain/Order.java
@@ -33,11 +33,13 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
+
+    //양방향에서는 연관관계 메서드가 있으면 좋음
+
     public void setMember(Member member){
         this.member =member;
         member.getOrders().add(this);
     }
- //양방향에서는 연관관계 메서드가 있으면 좋음
     public void addOrderItem(OrderItem orderItem){
         orderItems.add(orderItem);
         orderItem.setOrder(this);
@@ -48,4 +50,41 @@ public class Order {
         delivery.setOrder(this);
     }
 
+    //생성 메서드
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems){
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+
+        for(OrderItem orderItem: orderItems){
+            order.addOrderItem(orderItem);
+        }
+
+        order.setStatus(OrderStatus.order);
+        order.setOrderDate(LocalDateTime.now());
+
+        return order;
+
+    }
+
+    //비즈니스 로직
+    /**
+     * 주문 취소
+     * */
+
+    public void cancle(){
+        if (delivery.getStatus() == DeliveryStatus.Done)
+            throw new IllegalStateException("배송 완료된 상품은 취소가 불가능합니다.");
+
+        this.setStatus(OrderStatus.cancle);
+        for(OrderItem orderItem : orderItems){
+            orderItem.cancle();
+        }
+    }
+
+    public int getTotalPrice(){
+        return orderItems.stream()
+                .mapToInt(OrderItem::gettTotalPrice)
+                .sum();
+    }
 }
