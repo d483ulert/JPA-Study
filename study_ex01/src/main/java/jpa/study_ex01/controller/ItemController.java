f8b95dd3daf8f87ea33a91c5ace1b45ece41e664
jisung324 <jisung3245@gmail.com>
2022-09-11 21:10:49 +0900

    상품 수정

--- study_ex01/src/main/java/jpa/study_ex01/controller/ItemController.java
+++ study_ex01/src/main/java/jpa/study_ex01/controller/ItemController.java
@@ -1,11 +1,14 @@
 package jpa.study_ex01.controller;
 
 
+import jpa.study_ex01.item.Book;
 import jpa.study_ex01.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,5 +22,19 @@ public class ItemController {
         return "items/createItemForm";
     }
 
+
+
+    @PostMapping("items/{itemId}/edit")
+    public String updateitem(@ModelAttribute("form") BookForm form){
+
+        Book book = new Book();
+        book.setPrice(form.getPrice());
+        book.setName(form.getName());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        return "redirect:form";
+
+    }
 
 }
