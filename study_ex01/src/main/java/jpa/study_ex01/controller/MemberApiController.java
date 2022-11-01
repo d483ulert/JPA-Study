520cd51295cf77ae508c516305e847e55d1b0acd
d483ulert <tottenham3@naver.com>
2022-11-01 22:28:27 +0900

    GET

--- study_ex01/src/main/java/jpa/study_ex01/controller/MemberApiController.java
+++ study_ex01/src/main/java/jpa/study_ex01/controller/MemberApiController.java
@@ -7,8 +7,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,6 +18,28 @@ public class MemberApiController {
 
     private final MemberService memberService;
 
+
+    @GetMapping("/api/v2/members")
+    public Result memberV2(){
+        List<Member> memberList = memberService.findMembers();
+        List<MemberDto> memberDtos = memberList.stream()
+                .map(m-> new MemberDto(m.getName()))
+                .collect((Collectors.toList()));
+
+        return new Result(memberDtos);
+    }
+
+    @Data@AllArgsConstructor
+    static class Result<T>{
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto{
+        private String name;
+    }
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMembreV1(@RequestBody @Valid Member member){
