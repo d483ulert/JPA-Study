656c765c46b2aacaf46c09ba1fb38ee84e4572c5
d483ulert <tottenham3@naver.com>
2022-11-08 21:43:21 +0900

    간단한 주문 조회 V2: 엔티티를 DTO로 변환

--- study_ex01/src/main/java/jpa/study_ex01/controller/OrderSimpleApiController.java
+++ study_ex01/src/main/java/jpa/study_ex01/controller/OrderSimpleApiController.java
@@ -1,13 +1,20 @@
 package jpa.study_ex01.controller;
 
+import jpa.study_ex01.domain.Address;
 import jpa.study_ex01.domain.Order;
+import jpa.study_ex01.domain.OrderStatus;
 import jpa.study_ex01.repository.OrderRepository;
 import jpa.study_ex01.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static jpa.study_ex01.domain.OrderStatus.order;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,7 +22,7 @@ public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
 
-    @GetMapping("/api/v1/simple-orders")
+    /*@GetMapping("/api/v1/simple-orders")
     public List<Order> orderSV1(){
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
 
@@ -24,5 +31,30 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress();   //laze 강제초기화
         }
         return all;
+    }
+    */
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> orderV2(){
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        orders.stream()
+                .map(o-> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+    }
+
+    @Data
+    static class SimpleOrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order){
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            address = order.getDelivery().getAddress();
+        }
     }
 }
