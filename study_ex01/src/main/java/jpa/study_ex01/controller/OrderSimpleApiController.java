034526343fb48cfc2df09a53ddfca5406ea7fbea
d483ulert <tottenham3@naver.com>
2022-11-08 22:23:25 +0900

    간단한 주문 조회 V4: JPA에서 DTO로 바로 조회

--- study_ex01/src/main/java/jpa/study_ex01/controller/OrderSimpleApiController.java
+++ study_ex01/src/main/java/jpa/study_ex01/controller/OrderSimpleApiController.java
@@ -1,26 +1,20 @@
 package jpa.study_ex01.controller;
 
-import jpa.study_ex01.domain.Address;
-import jpa.study_ex01.domain.Order;
-import jpa.study_ex01.domain.OrderStatus;
 import jpa.study_ex01.repository.OrderRepository;
-import jpa.study_ex01.repository.OrderSearch;
-import lombok.Data;
+import jpa.study_ex01.repository.order.simplequery.SimpleOrderQueryDto;
+import jpa.study_ex01.repository.order.simplequery.OrderSimpleQueryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import static jpa.study_ex01.domain.OrderStatus.order;
 
 @RestController
 @RequiredArgsConstructor
 public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
 
     /*@GetMapping("/api/v1/simple-orders")
     public List<Order> orderSV1(){
@@ -41,27 +35,20 @@ public class OrderSimpleApiController {
                 .collect(Collectors.toList());
 
     }*/
-    @GetMapping("/api/v3/simple-orders")
+/*    @GetMapping("/api/v3/simple-orders")  // fetch join
     public List<SimpleOrderDto> orderV3(){
         List<Order> orders = orderRepository.findAllWithMemberDelivery();
         return orders.stream()
                 .map(o->new SimpleOrderDto(o))
                 .collect(Collectors.toList());
+    }*/
+
+    // V3(fetch join) 는 다양한곳에서 재사용 가능 , 하지만 V4는 성능은 V3보다 나을지 몰라도 재사용이 힘듬, 쿼리를 짠것과 비슷
+    @GetMapping("/api/v4/simple-orders")        //레파지토리는 순수하게 entity를 조회하는 용도로 사용
+    public List<SimpleOrderQueryDto> orderV4() {
+        return orderSimpleQueryRepository.findOrderDtos();
     }
 
-    @Data
-    static class SimpleOrderDto{
-        private Long orderId;
-        private String name;
-        private LocalDateTime orderDate;
-        private OrderStatus orderStatus;
-        private Address address;
 
-        public SimpleOrderDto(Order order){
-            orderId = order.getId();
-            name = order.getMember().getName();
-            orderDate = order.getOrderDate();
-            address = order.getDelivery().getAddress();
-        }
-    }
+
 }
