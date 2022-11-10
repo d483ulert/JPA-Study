7819fc0ff4bbd056850addc61f1491777321d1db
d483ulert <tottenham3@naver.com>
2022-11-10 22:10:42 +0900

    주문 조회 V3: 엔티티를 DTO로 변환 - 페치 조인 최적화   * 페이징 불가 warn

--- study_ex01/src/main/java/jpa/study_ex01/controller/OrderApiController.java
+++ study_ex01/src/main/java/jpa/study_ex01/controller/OrderApiController.java
@@ -10,6 +10,7 @@ import jpa.study_ex01.repository.order.simplequery.OrderSimpleQueryRepository;
 import jpa.study_ex01.repository.order.simplequery.SimpleOrderQueryDto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,7 +43,7 @@ public class OrderApiController {
         return all;
     }*/
 
-    @GetMapping("/api/v2/orders")
+   /* @GetMapping("/api/v2/orders")
     public List<OrderDto> ordersV2(){
         List<Order> orders = orderRepository.findAllByString(new OrderSearch());
 
@@ -51,7 +52,15 @@ public class OrderApiController {
                 .collect(Collectors.toList());
 
         return collect;
+    }*/
+
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> ordersV3(){
+        List<Order> orders = orderRepository.findAllWithItem();
+        return orders.stream().map(o->new OrderDto(o))
+                .collect(Collectors.toList());
     }
+
 
     @Getter
     static class OrderDto {
--- study_ex01/src/main/java/jpa/study_ex01/repository/OrderRepository.java
+++ study_ex01/src/main/java/jpa/study_ex01/repository/OrderRepository.java
@@ -31,4 +31,14 @@ public class OrderRepository {
                  "join fetch o.delivery d", Order.class).getResultList();
     }
 
+    public List<Order> findAllWithItem() {
+            // 일대다 fetch조인 시 페이징 쿼리 안나감 ㅍㅔ이징 불가!
+        return em.createQuery(
+                "select distinct o from Order o" +
+                        "join fetch o.member m" +
+                        "join fetch o.delevery d" +
+                        "join fetch o.orderItems oi" +
+                        "join fetch oi.item.i",Order.class
+                ).setFirstResult(1).setMaxResults(10).getResultList();
+    }
 }
