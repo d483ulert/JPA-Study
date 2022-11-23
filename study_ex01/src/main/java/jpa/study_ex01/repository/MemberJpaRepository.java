bb5d1df838bc8ab874267391b42364b2ee5b44d3
d483ulert <tottenham3@naver.com>
2022-11-23 17:28:53 +0900

    spring repository

--- study_ex01/src/main/java/jpa/study_ex01/repository/MemberJpaRepository.java
+++ study_ex01/src/main/java/jpa/study_ex01/repository/MemberJpaRepository.java
@@ -4,6 +4,8 @@ import jpa.study_ex01.entity.Member;
 import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class MemberJpaRepository {
@@ -16,8 +18,20 @@ public class MemberJpaRepository {
         return member;
     }
 
+    public Optional<Member> findById(Long id){
+        Member member = em.find(Member.class,id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count(){
+        return em.createQuery("select count(m) from Member m", Long.class).getSingleResult();
+    }
     public Member find(Long id){
         return em.find(Member.class,id);
+    }
+
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class).getResultList();
     }
 
 }
--- study_ex01/src/test/java/jpa/study_ex01/repository/MemberJpaRepositoryTest.java
+++ study_ex01/src/test/java/jpa/study_ex01/repository/MemberJpaRepositoryTest.java
@@ -6,6 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Transactional
@@ -25,7 +28,30 @@ class MemberJpaRepositoryTest {
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUser_name()).isEqualTo(member.getUser_name());
         assertThat(findMember).isEqualTo(member);
+    }
+    @Test
+    public void basicCRUD(){
+        Member member1 = new Member("김지성1");
+        Member member2 = new Member("김지성2");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
 
+        //단건조회
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+
+        System.out.println("findMember1,2");
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        //리스트 조회 검증
+        System.out.println("********************");
+        List<Member> all = memberJpaRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        long count = memberJpaRepository.count();
+        assertThat(count).isEqualTo(2);
 
     }
+
 }
